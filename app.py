27a52a2102a606057fd0ed776907dcaedf335781
MASTER0131 <ufukcambaz4@gmail.com>
2026-01-04 14:46:06 +0300

    Change DATABASE_URL to use a placeholder
    Updated DATABASE_URL variable to use a placeholder for better security.

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import psycopg2
 app = Flask(__name__)
 
 # Render'ın otomatik tanımladığı veritabanı bağlantı bilgisi (DATABASE_URL ortam değişkeni)
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://volkan:201eAcwsVd7cf1jyxQL2F1wn4VZ3FSbW@dpg-d3t90iur433s73b5q070-a.oregon-postgres.render.com/cloud_db_gwjr")
+DATABASE_URL = os.getenv("DATABASE_URL", # Hatalı/Eski hali
+db_url = "postgres://user:password@dpg-d3tj...oregon-postgres.render.com/dbname")
 
 # HTML ŞABLONU (tek sayfada form + liste)
 HTML = """
